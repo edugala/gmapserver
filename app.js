d1e42cafad570473da26b50deec47d9598b8a3b8
rahulmishra <rahulmishra.hello@gmail.com>
2017-07-07 13:07:27 +0530

    none:

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
-
+app.get('/', function(req, res){
+	res.send({"msg": "Root Request Received"})
+})
 app.listen(3000, function(){
 	console.log("Server now listening to port 3000")
 })
